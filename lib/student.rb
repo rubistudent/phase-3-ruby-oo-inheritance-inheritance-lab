87298ddcb6098e8b1de47f031307f36487470072
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-18 18:40:00 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student 
 
 end
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize 
+      super
+      @knowledge=[]
+    end
+
+    def learn(addst)
+      @knowledge = addst
+    end
+  end
